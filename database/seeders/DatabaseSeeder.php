a246ee1cea0646db5305f73b8ec46e75bcff4e7c
Md Jahirul islam <mdjoherul50@gmail.com>
2025-10-30 16:53:02 +0600

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,13 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
@@ -17,9 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // SuperAdminSeeder কে কল করুন
+        $this->call([
+            SuperAdminSeeder::class,
         ]);
     }
 }
